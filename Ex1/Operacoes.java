706c82992389392c7852842a673e3c06d34d2e8d
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-21 14:23:41 -0300

    Código Operações

--- Ex1/Operacoes.java
+++ Ex1/Operacoes.java
@@ -1,1 +1,34 @@
+public class Operacoes {
+    
+    int valor1 = 0;
+    int valor2 = 0;
 
+
+    static void Soma(int valor1, int valor2){
+        
+        double resultado = valor1 + valor2;
+
+        System.out.println("O resultado é: " + resultado);
+    }
+
+    static void Subtracao(int valor1, int valor2){
+
+        double resultado = valor1 - valor2;
+
+        System.out.println("O resultado é: " + resultado);
+    }
+
+    static void Divisao(int valor1, int valor2){
+
+        double resultado = valor1 / valor2;
+        
+        System.out.println("O resultado é: " + resultado);
+    }
+
+    static void Multiplicacao(int valor1, int valor2){
+
+        double resultado = valor1 * valor2;
+        
+        System.out.println("O resultado é: " + resultado);
+    }
+}
