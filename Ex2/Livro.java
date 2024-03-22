e8bd4184ae90fa06ec0d8e0dccc85c9f57cb46bd
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-22 12:27:01 -0300

    Update Livro.java

--- Ex2/Livro.java
+++ Ex2/Livro.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+
+public class Livro {
+    
+    static ArrayList<String> Nome;
+    static ArrayList<String> Titulo;
+    static ArrayList<Integer> Ano;
+
+    public Livro(String nome2, String titulo2, int ano2) {
+        this.Nome = new ArrayList<>();
+        this.Titulo = new ArrayList<>();
+        this.Ano = new ArrayList<>(); 
+    }
+
+    static void ExibirInfo(){
+
+        System.out.println("-- Titulo:" + Titulo);
+        System.out.println("-- Autor: " + Nome);
+        System.out.println("-- Ano de Publicação: " + Ano);
+
+    }
+
+}
 
