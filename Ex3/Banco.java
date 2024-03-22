995577f2326abfc984c9b5afec7cfae2e8409ee2
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-21 21:08:51 -0300

    Update Banco.java

--- Ex3/Banco.java
+++ Ex3/Banco.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner leitor = new Scanner(System.in);
+        List<ContaBancaria> contas = new ArrayList<ContaBancaria>();
+        int opcao=0;
+
+        System.out.println("-- Bem vindo--");
+        System.out.println("O que você deseja realizar?");
+        System.out.println("1. Depósito");
+        System.out.println("2. Saque");
+        System.out.println("3. Ver saldo");
+        System.out.println("4. Criar conta");
+        opcao = leitor.nextInt();
+
+        switch(opcao){
+            case 1: 
+            
+            break;
+            case 2:  
+
+            break;
+            case 3:
+
+            break;
+            case 4: 
+
+            break;
+            default:
+                System.out.println("Opção inválida!");
+            break;
+        }
+
+
+        leitor.close();
+    }
+}
 
