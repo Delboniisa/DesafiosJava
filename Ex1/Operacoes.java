62cf2b6fd2c0a6b06f6da0f10c15c83443e49f17
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-29 11:04:33 -0300

    Classe Operacoes atualizada

--- Ex1/Operacoes.java
+++ Ex1/Operacoes.java
@@ -1,3 +1,4 @@
+
 public class Operacoes {
     
     int valor1 = 0;
@@ -20,9 +21,12 @@ public class Operacoes {
 
     static void Divisao(int valor1, int valor2){
 
-        double resultado = valor1 / valor2;
-        
-        System.out.println("O resultado é: " + resultado);
+        if (valor2 != 0) {
+            double resultado = valor1 / valor2;
+            System.out.println("O resultado é: " + resultado);
+        } else {
+            System.out.println("Erro! não é possível dividir por zero");
+        }
     }
 
     static void Multiplicacao(int valor1, int valor2){
