8c7c060f8fe9dac9fe1b19e7eea87e854f65d4f6
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-29 13:14:52 -0300

    Atualização da classe jogador

--- Ex4/Jogador.java
+++ Ex4/Jogador.java
@@ -1,1 +1,25 @@
+public class Jogador {
+    private String nome;
+    private int pontuacao;
+    private int nivel;
 
+    public Jogador(String nome, int pontuacao, int nivel) {
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+        this.nivel = nivel;
+    }
+
+    public void aumentarPontuacao(int pontos) {
+        this.pontuacao += pontos;
+    }
+
+    public void subirNivel() {
+        this.nivel++;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Pontuação: " + pontuacao);
+        System.out.println("Nível: " + nivel);
+    }
+}
