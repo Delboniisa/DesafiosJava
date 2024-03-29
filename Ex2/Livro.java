d4596282a02c89ae76fcdf27f7d2d5a29f5428b6
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-29 11:32:40 -0300

    Atualização da classe Livro

--- Ex2/Livro.java
+++ Ex2/Livro.java
@@ -1,24 +1,19 @@
-import java.util.ArrayList;
-
-public class Livro {
+class Livro {
     
-    static ArrayList<String> Nome;
-    static ArrayList<String> Titulo;
-    static ArrayList<Integer> Ano;
+    private String Nome;
+    private String Titulo;
+    private int Ano;
 
-    public Livro(String nome2, String titulo2, int ano2) {
-        this.Nome = new ArrayList<>();
-        this.Titulo = new ArrayList<>();
-        this.Ano = new ArrayList<>(); 
+    public Livro(String nome, String titulo, int ano) {
+        this.Nome = nome;
+        this.Titulo = titulo;
+        this.Ano = ano;
     }
 
-    static void ExibirInfo(){
-
-        System.out.println("-- Titulo:" + Titulo);
-        System.out.println("-- Autor: " + Nome);
-        System.out.println("-- Ano de Publicação: " + Ano);
-
+    public void ExibirInfo() {
+        System.out.println("-- Título: " + this.Titulo);
+        System.out.println("-- Autor: " + this.Nome);
+        System.out.println("-- Ano de Publicação: " + this.Ano);
+        System.out.println();
     }
-
 }
-
