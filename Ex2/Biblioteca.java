f023799564363274da999ad12f89d4f5455cfbe1
Delboniisa <144742299+Delboniisa@users.noreply.github.com>
2024-03-29 11:31:50 -0300

    Atualização da classe biblioteca

--- Ex2/Biblioteca.java
+++ Ex2/Biblioteca.java
@@ -7,47 +7,52 @@ public class Biblioteca {
     public static void main(String[] args) {
         
         Scanner leitor = new Scanner(System.in);
+        String respostaUsuario;
 
-        List <Livro> estante = new ArrayList<>();
+        List<Livro> estante = new ArrayList<>();
         int size = 0;
 
         System.out.println("-- Bem Vindo(a) a Biblioteca! --");
-        System.out.println("1. Adicionar livros:");
-        System.out.println("2. Listar livros:");
-        int opcao = leitor.nextInt();
+        
+        do {
+            System.out.println("1. Adicionar livros:");
+            System.out.println("2. Listar livros:");
+            int opcao = leitor.nextInt();
+            leitor.nextLine(); 
 
-        switch(opcao){
-            case 1:
-                System.out.println("Deseja adicionar informações de quantos livros: ");
-                size = leitor.nextInt();
-
-                System.out.println("Insira as informações dos livros: ");
-
-                for(int i=0; i<size; i++){
-
+            switch(opcao) {
+                case 1:
+                    System.out.println("Deseja adicionar informações de quantos livros: ");
+                    size = leitor.nextInt();
                     leitor.nextLine();
-                    System.out.println("Nome do Livro:");
-                    String titulo = leitor.nextLine();
-            
-                    System.out.println("Nome do Autor: ");
-                    String nome = leitor.nextLine();
-            
-                    System.out.println("Ano de publicação: ");
-                    int ano = leitor.nextInt();
-                    
-                    Livro livro = new Livro(nome, titulo, ano);
-                    estante.add(livro);
-                }
-            break;
-            case 2:
-                for(int i=0; i<size; i++){
 
-                    Livro.ExibirInfo();
-                }
-            break;
-        }
+                    System.out.println("Insira as informações dos livros: ");
 
+                    for(int i = 0; i < size; i++) {
+                        System.out.println("Nome do Livro:");
+                        String titulo = leitor.nextLine();
+                
+                        System.out.println("Nome do Autor: ");
+                        String nome = leitor.nextLine();
+                
+                        System.out.println("Ano de publicação: ");
+                        int ano = leitor.nextInt();
+                        leitor.nextLine();
+                        
+                        Livro livro = new Livro(nome, titulo, ano);
+                        estante.add(livro);
+                    }
+                    break;
+                case 2:
+                    for (Livro livro : estante) {
+                        livro.ExibirInfo();
+                    }
+                    break;
+            }
+            System.out.println("Deseja continuar (s/n)?");
+            respostaUsuario = leitor.nextLine();
+        } while(respostaUsuario.equals("s"));
+        
         leitor.close();
     }
 }
-
