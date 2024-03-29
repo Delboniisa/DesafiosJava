f6e02b88895cb555d145280069df9bc22f47e30c
Delboniisa <isabela.delboni@gmail.com>
2024-03-29 20:18:21 -0300

    Ex7

--- Ex7/Estoque.java
+++ Ex7/Estoque.java
@@ -1,5 +1,42 @@
-package Ex7;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Estoque {
     
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+
+        List<Produto> estoque = new ArrayList<>();
+
+        while (true) {
+            System.out.println("--- Estoque ---");
+            System.out.println("1. Adicionar novo produto ");
+            System.out.println("2. Atualizar um produto ");
+            System.out.println("3. Remover um produto ");
+            System.out.println("4. Imprimir relatório ");
+
+            int opcao = leitor.nextInt();
+            leitor.nextLine(); 
+
+            switch(opcao){
+                case 1: 
+                    Produto.adicionarProduto(leitor, estoque);
+                    break;
+                case 2:
+                    Produto.atualizarProduto(leitor, estoque);
+                    break;
+                case 3: 
+                    Produto.removerProduto(leitor, estoque);
+                    break;
+                case 4: 
+                    Produto.imprimirRelatorio(estoque);
+                    break;
+                default: 
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+    }
 }
--- Ex7/Produto.java
+++ Ex7/Produto.java
@@ -1,5 +1,150 @@
-package Ex7;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 public class Produto {
+    private String nome;
+    private float preco;
+    private int quantidade;
+    private int codigo;
+
+    public Produto(String nome, float preco, int quantidade, int codigo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
     
+    //modificara variavel
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public static void adicionarProduto(Scanner leitor, List<Produto> estoque) {
+        System.out.println("Nome do produto: ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Quantidade do produto: ");
+        int quantidade = leitor.nextInt();
+
+        System.out.println("Preço do produto: ");
+        float preco = leitor.nextFloat();
+
+        System.out.println("Código do produto: ");
+        int codigo = leitor.nextInt();
+
+        boolean codigoDuplicado = false;
+        for (int i = 0; i < estoque.size(); i++) {
+            Produto produto = estoque.get(i);
+            if (produto.getCodigo() == codigo) {
+                codigoDuplicado = true;
+                break;
+            }
+        }
+
+        if (codigoDuplicado) {
+            System.out.println("Erro, já existe um produto com esse código!");
+        } else {
+            Produto produto = new Produto(nome, preco, quantidade, codigo);
+            estoque.add(produto);
+            System.out.println("Produto adicionado com sucesso!");
+        }
+    }
+
+    public static void atualizarProduto(Scanner leitor, List<Produto> estoque) {
+        System.out.println("Digite o código do produto que deseja atualizar: ");
+        int codigo = leitor.nextInt();
+        leitor.nextLine(); // Limpar o buffer do teclado
+
+        boolean produtoEncontrado = false;
+        for (Produto produto : estoque) {
+            if (produto.getCodigo() == codigo) {
+                produtoEncontrado = true;
+
+                System.out.println("Digite o novo nome do produto: ");
+                String novoNome = leitor.nextLine();
+
+                System.out.println("Digite o novo preço do produto: ");
+                float novoPreco = leitor.nextFloat();
+
+                System.out.println("Digite a nova quantidade do produto: ");
+                int novaQuantidade = leitor.nextInt();
+
+                produto.setNome(novoNome);
+                produto.setPreco(novoPreco);
+                produto.setQuantidade(novaQuantidade);
+
+                System.out.println("Produto atualizado com sucesso!");
+                break;
+            }
+        }
+
+        if (!produtoEncontrado) {
+            System.out.println("Produto não encontrado.");
+        }
+    }
+
+    public static void removerProduto(Scanner leitor, List<Produto> estoque) {
+        System.out.println("Digite o código do produto que deseja remover: ");
+        int codigo = leitor.nextInt();
+        leitor.nextLine();
+
+        boolean produtoEncontrado = false;
+        Iterator<Produto> iterator = estoque.iterator();
+        while (iterator.hasNext()) {
+            Produto produto = iterator.next();
+            if (produto.getCodigo() == codigo) {
+                iterator.remove();
+                produtoEncontrado = true;
+                System.out.println("Produto removido com sucesso!");
+                break;
+            }
+        }
+
+        if (!produtoEncontrado) {
+            System.out.println("Produto não encontrado.");
+        }
+    }
+
+    public static void imprimirRelatorio(List<Produto> estoque) {
+        System.out.println("--- Relatório de Estoque ---");
+        float valorTotal = 0;
+        for (int i = 0; i < estoque.size(); i++) {
+            Produto produto = estoque.get(i);
+            System.out.println("Nome: " + produto.getNome());
+            System.out.println("Preço: " + produto.getPreco());
+            System.out.println("Quantidade: " + produto.getQuantidade());
+            float valorProduto = produto.getPreco() * produto.getQuantidade();
+            System.out.println("Valor total do produto: " + valorProduto);
+            valorTotal += valorProduto;
+            System.out.println("Código: " + produto.getCodigo());
+            System.out.println("-----------------------------");
+        }
+        System.out.println("Valor total do estoque: " + valorTotal);
+    }
 }
